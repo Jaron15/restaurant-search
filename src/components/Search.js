0926fff4d54d671fbaa191220d8557ba4b208bfc
Jaron <jjkenyon777@gmail.com>
2022-05-19 15:07:30 -0700

    search component

---
+++ src/components/Search.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TextInput, View, StyleSheet} from 'react-native';
+import {FontAwesome} from "@expo/vector-icons";
+import {elevation} from '../common/styles';
+
+function Search() {
+    return (
+        <View style={[styles.container, styles.elevation]} >
+            <FontAwesome name="search" size={25} />
+            <TextInput
+            style={styles.input}
+            placeholder='Restaurants, food' />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginTop: 5,
+        marginHorizontal: 25,
+        backgroundColor: "white",
+        padding: 15,
+        borderRadius: 40
+    },
+    elevation,
+    input: {
+        fontSize: 20,
+        marginLeft: 10
+    }
+})
+
+export default Search;
